22e8152ac3f6e91d17352148236a844f37ffbbc5
AlexOcegueda <alexocegueda59@gmail.com>
2022-11-12 20:22:57 -0600

    removed unused imports

--- src/components/NavBar/NavBarStyle.js
+++ src/components/NavBar/NavBarStyle.js
@@ -1,6 +1,4 @@
 import styled from 'styled-components';
-import { Contanier } from '../../globalStyle';
-import { Link } from 'react-router-dom';
 
 export const Nav = styled.nav`
     display: flex;
