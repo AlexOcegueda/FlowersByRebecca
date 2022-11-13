37566cd1a8b06778c9b01a5e02608b2275a1a653
AlexOcegueda <alexocegueda59@gmail.com>
2022-11-12 18:53:57 -0600

    Added Routing to pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyle from './globalStyle';
+import {Routes, Route} from 'react-router-dom'
+
+// pages
+import Home from './pages/Home';
+import Services from './pages/Services';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>  
+      <GlobalStyle />
+      <Routes>\
+        <Route path="/" element={<Home />}/>
+        <Route path="/Services" element={<Services />}/>
+      </Routes>
     </div>
   );
 }
