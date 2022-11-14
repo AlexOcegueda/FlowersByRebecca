34078f5ddac23dcfacbf7ad8016ce4d7ead63b2c
AlexOcegueda <alexocegueda59@gmail.com>
2022-11-14 01:58:07 -0600

    Added dropdown menu to nav bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,19 +1,40 @@
 import React from 'react';
-import {Nav, LeftNav, RightNav, NavItem, WebsiteName} from './NavBarStyle';
+import './NavBarStyle.css';
+//import { Nav, DropDownContainer, DropDownText, DropDownBtn, DropDownMenu, DropDownItem } from './NavBarStyle';
 
-export default function NavBar() {
-  return (
+export default function NavBar(props) {
+    
+    return (
+        
         <>
-            <Nav>
-                <LeftNav>
-                    <WebsiteName>Flowers By Rebecca</WebsiteName>
-                    <NavItem to="/">Home</NavItem>
-                    <NavItem to="/Services">Services</NavItem>
-                </LeftNav>
-                <RightNav>
-                </RightNav>
-            </Nav>
-        </>
+            <nav>
+                    <div class="dropdown">
+                        <div class="products">
+                        <button className='menu'>Menu</button>
+                            <ul>
+                                <li><a href="#">Services</a></li>
+                                <li><a href="#">About Me</a></li>
+                            </ul>
+                        </div>
+                    </div>
+            </nav>
+       </>
 
     )
+
+    
 }
+
+/* 
+
+<Nav>
+            <DropDownContainer class="dropdown">
+                <DropDownText class="projects">
+                    <DropDownBtn>Menu</DropDownBtn>
+                    <DropDownMenu>
+                        <DropDownItem>Services</DropDownItem>
+                    </DropDownMenu>
+                </DropDownText>
+            </DropDownContainer>
+        </Nav>
+*/
