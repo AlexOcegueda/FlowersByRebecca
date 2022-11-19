d30debe8caa1e66a3c4360788f22fa73ee801d30
AlexOcegueda <alexocegueda59@gmail.com>
2022-11-18 20:05:33 -0600

    merged styles with js file

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import NavBar from '../components/NavBar/NavBar';
-import Footer from '../components/Footer/FooterBar';
-import {Heading} from './ContactStyle';
 
 
 export default function ContactMe() {
   return (
     <>
-        <NavBar />
-        <Heading>Contact Me</Heading>
-        <Footer />
     </>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import NavBar from '../components/NavBar/NavBar';
-import Footer from '../components/Footer/FooterBar'
-import {BannerContainer, BannerText,
-        ContentContainer, ContentItem, Contact} from './HomeStyle'
+import styled from "styled-components";
+import {Link} from 'react-router-dom';
 
 export default function Home() {
   return (
     <>
-      <NavBar />
         <BannerContainer>
           <BannerText>Flowers By Rebecca</BannerText>
           <Contact to="/Contact">Contact Me</Contact>
@@ -22,7 +19,72 @@ export default function Home() {
           <ContentItem src={require("./BeckysFlowers/home5.jpg")}></ContentItem>
           <ContentItem src={require("./BeckysFlowers/home6.jpg")}></ContentItem>
         </ContentContainer>
-      <Footer />
     </>
   )
 }
+
+export const BannerContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media (max-width: 810px){
+        padding: 0px 35px 0px 35px;
+    }
+`;
+
+export const BannerText = styled.p`
+    font-weight: 500;
+    font-size: 120px;
+    font-family: 'Simonetta', cursive;
+    padding-left: 80px;
+    @media (max-width: 810px){
+        font-size: 65px;
+    }
+`;
+
+export const Contact = styled(Link)`
+    padding: 10px;
+    background-color: rgb(82,31,31);
+    border-radius: 2rem;
+    margin-right: 8px;
+    color: white;
+    font-weight: 600;
+    font-size: 12px;
+    text-decoration: none;
+
+    :hover {
+        background-color: white;
+        color: black;
+        cursor: pointer;
+    }
+
+    @media (max-width: 810px) {
+        font-size: 12px;
+        margin-right: 0px; // this controls orientation of Bold Name
+        padding: 6px;
+    }
+`;
+
+export const ContentContainer = styled.div`
+    padding: 20px;
+    display: grid;
+    grid-template-columns: auto auto auto auto;
+    row-gap: 10px;
+    column-gap: 10px;
+    
+    @media (max-width: 810px) {
+        grid-template-columns: auto auto;
+    }
+`;
+
+export const ContentItem = styled.img`
+    width: 98%;
+    height: 98%;
+
+    :hover {
+        width: 100%;
+        height: 100%;
+        cursor: pointer;
+    }
+
+`;
