e41bd9d3660c72771f340d886c22a15a3232a85f
AlexOcegueda <alexocegueda59@gmail.com>
2022-11-18 20:05:55 -0600

    Made nav and footer by a contant on all app

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import './App.css'
 import Home from './pages/Home';
 import Services from './pages/Services';
 import ContactMe from './pages/ContactMe';
+import NavBar from './components/NavBar/NavBar';
+import FooterBar from './components/Footer/FooterBar';
 
 function App() {
   return (
-    <div className='App'>  
+    <div className='App'> 
+    <NavBar />
       <Routes>\
         <Route path="/" element={<Home />}/>
         <Route path="/Services" element={<Services />}/>
         <Route path="/Contact" element={<ContactMe />}/>
       </Routes>
+    <FooterBar />
     </div>
   );
 }
