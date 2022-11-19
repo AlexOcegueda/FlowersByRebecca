f7b8218545d06a7896b2e6d1fe1410cf0a847225
AlexOcegueda <alexocegueda59@gmail.com>
2022-11-18 19:53:13 -0600

    Added Logo to nav

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,6 +7,7 @@ export default function NavBar() {
     return (
         <>
             <Nav>
+                <Logo>Flowers By Rebecca</Logo>
                 <NavItem to="/">Home</NavItem>
                 <NavItem to="/Services">Services</NavItem>
                 <NavItem to="/Contact">Contact</NavItem>
@@ -20,7 +21,7 @@ export default function NavBar() {
 export const Nav = styled.nav`
     display: flex;
     border-bottom: 1px solid black;
-
+    align-items: center;
     @media (max-width: 500px) {
         justify-content: space-between;
     }
@@ -30,11 +31,22 @@ export const NavItem = styled(Link)`
     text-decoration: none;
     padding: 10px;
     margin: 10px;
-    font-weight: 600;
+    font-weight: 400;
     font-size: 20px;
     color black;
     font-family: 'Simonetta', cursive;
     :hover {
         color: white;
     }
+`;  
+
+export const Logo = styled(Link)`
+    text-decoration: none;
+    padding: 10px;
+    margin: 10px;
+    font-weight: 900;
+    font-size: 30px;
+    color black;
+    font-family: 'Simonetta', cursive;
+
 `;  
