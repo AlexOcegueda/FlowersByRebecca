4f1ad69744be1eaea7acb97b023eef41c3076be5
AlexOcegueda <alexocegueda59@gmail.com>
2022-11-18 20:37:10 -0600

    removed double header and footer in services

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import NavBar from '../components/NavBar/NavBar';
-import Footer from '../components/Footer/FooterBar';
 
 export default function Services() {
   return (
     <>
-      <NavBar />
-      <h1>Services</h1>
-      <Footer />
     </>
   )
 }
