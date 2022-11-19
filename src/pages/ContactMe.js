8b06a26dd99603770eee07b4f27c24c1bfeb53b2
AlexOcegueda <alexocegueda59@gmail.com>
2022-11-18 19:22:39 -0600

    provided boilerplate for future pages

---
+++ src/pages/ContactMe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from '../components/NavBar/NavBar';
+import Footer from '../components/Footer/FooterBar';
+import {Heading} from './ContactStyle';
+
+
+export default function ContactMe() {
+  return (
+    <>
+        <NavBar />
+        <Heading>Contact Me</Heading>
+        <Footer />
+    </>
+  )
+}
---
+++ src/pages/Services.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../components/NavBar/NavBar';
+import Footer from '../components/Footer/FooterBar';
+
+export default function Services() {
+  return (
+    <>
+      <NavBar />
+      <h1>Services</h1>
+      <Footer />
+    </>
+  )
+}
